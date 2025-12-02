b6c27ed9616b7584aacb9ec745d61becf4541364
Polkey <97398666+Polkey@users.noreply.github.com>
2025-12-02 13:13:24 +0200

    Some updates to AI code, more tiling tests and changes to map. Continuing work from home.

--- Assets/MikkoFolder/BasicEnemy.cs
+++ Assets/MikkoFolder/BasicEnemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class BasicEnemy : MonoBehaviour
 {
@@ -8,7 +9,14 @@ public class BasicEnemy : MonoBehaviour
     public GameObject Projectile;
     public float firingInterval;
     public float projectileSpeed;
+    public float projectileLifetime;
+    public int health = 50;
+    public float detectionRange = 120;
+    public float detectionAngle = 45f;
+    Quaternion rotation;
+    float distanceToPlayer;
     float timer;
+    bool detectPlayer = false;
     void Start()
     {
         
@@ -17,17 +25,12 @@ public class BasicEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        var rotation = Quaternion.LookRotation(Target.position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime* dampening);
-        if (Input.GetKeyDown(KeyCode.W)) {
-            fireWeapon();
-        }
-       while (timer > firingInterval) {
 
-            fireWeapon();
-            timer -= firingInterval;
-        }
+
+        distanceToPlayer = Vector3.Distance(this.transform.position, Target.position);
+        rotation = Quaternion.LookRotation(Target.position - transform.position);
+        searchForPlayer();
+
     }
     private void fireWeapon() {
 
@@ -35,5 +38,31 @@ public class BasicEnemy : MonoBehaviour
         var irb = projectileInstance.GetComponent<Rigidbody>();
         irb.AddForce(transform.forward*projectileSpeed);
 
+
+    }
+    private void searchForPlayer() {
+        var delta = Target.position - transform.position;
+        float angle = Vector3.Angle(transform.forward, delta);
+        while (distanceToPlayer < detectionRange) {
+            if (detectPlayer) {
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * dampening);
+                timer += Time.deltaTime;
+                while (timer > firingInterval) {
+                    fireWeapon();
+                    timer -= firingInterval;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        var clockwise = Quaternion.Euler(0, -detectionAngle, 0);
+        var counterClockwise = Quaternion.Euler(0, detectionAngle, 0);
+        var longForward = transform.forward * detectionRange;
+        var left = counterClockwise * longForward;
+        var right = clockwise * longForward;
+        var p = transform.position;
+        Debug.DrawLine(p, p + left);
+        Debug.DrawLine(p, p + right);
     }
 }
