79d17a831d7457a6b0831c88001bed2ee6d72653
Polkey <mikkom.96@windowslive.com>
2025-12-02 18:56:29 +0200

    Implemented vision to AI with ranges and angles as well as a simple alarm system to trigger nearby enemies to turn around if one enemy spots the player.

--- Assets/MikkoFolder/BasicEnemy.cs
+++ Assets/MikkoFolder/BasicEnemy.cs
@@ -3,35 +3,53 @@ using static UnityEditor.PlayerSettings;
 
 public class BasicEnemy : MonoBehaviour
 {
-    public Transform Target;
+    Transform Target;
     public float dampening;
     public Transform firingPosition;
     public GameObject Projectile;
+    public float movementspeed;
     public float firingInterval;
     public float projectileSpeed;
-    public float projectileLifetime;
     public int health = 50;
     public float detectionRange = 120;
     public float detectionAngle = 45f;
+    public float alarmRange = 30;
     Quaternion rotation;
     float distanceToPlayer;
     float timer;
     bool detectPlayer = false;
     void Start()
     {
-        
+        Target = GameObject.FindAnyObjectByType<PlayerController>().transform;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
 
         distanceToPlayer = Vector3.Distance(this.transform.position, Target.position);
         rotation = Quaternion.LookRotation(Target.position - transform.position);
         searchForPlayer();
 
-    }
+        if (detectPlayer) {
+            var nearbyAI = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (var ai in nearbyAI) {
+                if(Vector3.Distance(this.transform.position, ai.transform.position) < alarmRange) {
+                    ai.transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * dampening);
+                }
+
+            }
+                //Rotating around target, kinda broken gonna figure out another way.
+                //transform.RotateAround(Target.position, Vector3.up, movementspeed * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * dampening);
+                timer += Time.deltaTime;
+                while (timer > firingInterval) {
+                    fireWeapon();
+                    timer -= firingInterval;
+                }
+            }
+
+        }
     private void fireWeapon() {
 
         GameObject projectileInstance = Instantiate(Projectile, firingPosition.position, transform.rotation);
@@ -43,16 +61,9 @@ public class BasicEnemy : MonoBehaviour
     private void searchForPlayer() {
         var delta = Target.position - transform.position;
         float angle = Vector3.Angle(transform.forward, delta);
-        while (distanceToPlayer < detectionRange) {
-            if (detectPlayer) {
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * dampening);
-                timer += Time.deltaTime;
-                while (timer > firingInterval) {
-                    fireWeapon();
-                    timer -= firingInterval;
-                }
-            }
-        }
+        if (distanceToPlayer < detectionRange && angle < detectionAngle) {
+            detectPlayer = true;
+        }else detectPlayer = false;
     }
 
     private void OnDrawGizmos() {
