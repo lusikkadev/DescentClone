6c947d08117698cbc1ff179cc7ba7b07bc332cbb
lusikkadev <tatuketonen@gmail.com>
2025-12-15 22:29:37 +0200

    Tweaks and main menu

--- Assets/TatunFolder/Scripts/Weapons/Projectile.cs
+++ Assets/TatunFolder/Scripts/Weapons/Projectile.cs
@@ -44,6 +44,7 @@ public class Projectile : MonoBehaviour
         if (((1 << collision.gameObject.layer) & hitMask) == 0) return;
         rb.linearVelocity = Vector3.zero;
         hitShader.Shoot(collision.contacts[0].point);
+        AudioFW.Play(id: "MissileExplosion");
         // enable shockwave
         if (shockWave != null)
         {
@@ -67,26 +68,84 @@ public class Projectile : MonoBehaviour
             }
         }
 
-        explosionEffect?.Play();
-        StartCoroutine(DestroyAfterEffect());
-    }
+        // Prepare explosion and trail particle systems in a way that avoids reusing shared prefab instances
+        ParticleSystem spawnedExplosion = null;
+        ParticleSystem spawnedTrail = null;
 
-    IEnumerator DestroyAfterEffect()
-    {
-        // stop trail
-        if (trailEffect != null)
-        {
-            trailEffect.transform.parent = null;
-            var trailMain = trailEffect.main;
-            trailMain.stopAction = ParticleSystemStopAction.Destroy;
-            trailEffect.Stop();
-        }
-        // wait for explosion effect to finish
         if (explosionEffect != null)
         {
-            var explosionMain = explosionEffect.main;
-            yield return new WaitForSeconds(explosionMain.duration + explosionMain.startLifetime.constantMax);
+            if (explosionEffect.transform.IsChildOf(transform))
+            {
+                // this projectile has its own explosion instance: detach and play
+                spawnedExplosion = explosionEffect;
+                spawnedExplosion.transform.parent = null;
+                spawnedExplosion.Play();
+            }
+            else
+            {
+                // explosionEffect refers to a shared asset: instantiate a copy at contact point
+                var go = Instantiate(explosionEffect.gameObject, collision.contacts[0].point, Quaternion.identity);
+                spawnedExplosion = go.GetComponent<ParticleSystem>();
+                if (spawnedExplosion != null) spawnedExplosion.Play();
+            }
         }
+
+        if (trailEffect != null)
+        {
+            if (trailEffect.transform.IsChildOf(transform))
+            {
+                // detach running trail so it continues independently
+                spawnedTrail = trailEffect;
+                spawnedTrail.transform.parent = null;
+                var trailMain = spawnedTrail.main;
+                trailMain.stopAction = ParticleSystemStopAction.Destroy;
+                spawnedTrail.Stop();
+            }
+            else
+            {
+                // shared trail asset - instantiate a copy at current position
+                var tgo = Instantiate(trailEffect.gameObject, trailEffect.transform.position, trailEffect.transform.rotation);
+                spawnedTrail = tgo.GetComponent<ParticleSystem>();
+                if (spawnedTrail != null)
+                {
+                    var trailMain2 = spawnedTrail.main;
+                    trailMain2.stopAction = ParticleSystemStopAction.Destroy;
+                    spawnedTrail.Stop();
+                }
+            }
+        }
+
+        StartCoroutine(DestroyAfterEffect(spawnedExplosion, spawnedTrail));
+    }
+
+    IEnumerator DestroyAfterEffect(ParticleSystem spawnedExplosion, ParticleSystem spawnedTrail)
+    {
+        // If trail was detached/instantiated above, ensure it will be destroyed when finished
+        if (spawnedTrail != null)
+        {
+            // already set stopAction and stopped above
+        }
+
+        // wait for explosion effect to finish
+        if (spawnedExplosion != null)
+        {
+            var explosionMain = spawnedExplosion.main;
+            float startLifetime = 0f;
+            if (explosionMain.startLifetime.mode == ParticleSystemCurveMode.TwoConstants || explosionMain.startLifetime.mode == ParticleSystemCurveMode.TwoCurves)
+                startLifetime = explosionMain.startLifetime.constantMax;
+            else
+                startLifetime = explosionMain.startLifetime.constant;
+
+            yield return new WaitForSeconds(explosionMain.duration + startLifetime + 0.05f);
+            // destroy spawned explosion object
+            Destroy(spawnedExplosion.gameObject);
+        }
+        else
+        {
+            // no explosion: small delay to allow other effects
+            yield return new WaitForSeconds(0.05f);
+        }
+
         Destroy(gameObject);
     }
 }
